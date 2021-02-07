d76d440949ab6fd1486aa6d716c1b9d1c89a0b05
Yunus Pazarlı <yunuspazarli@gmail.com>
2021-02-07 17:08:00 +0300

    template sample finished

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Buyukharfolmali from './components/Buyukharfolmali';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Pagecontent from './components/Pagecontent';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Henloo <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Pazarlı</p>
-        <ul>
-          
-            Yunus
-         <li>pazy</li>
-        </ul>
-        <Buyukharfolmali></Buyukharfolmali>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+ <Navbar></Navbar>
+
+<Header></Header>
+<Pagecontent></Pagecontent>
+<Footer></Footer>
+   </div>
+       
+        
   );
 }
 
